e3704a5da99eb8ce510e05ed29a4ebf5a5408735
the null <xinyechenai@gmail.com>
2024-01-19 11:58:10 +0100

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -3,18 +3,28 @@ from sklearn.cluster._k_means_common import _inertia_dense
 from distance import *
 from seeding import *
 
+
 class StandardKMeans:
-    def __init__(self, n_clusters, max_iters=100, tol=1e-4, random_state=42):
+    def __init__(self, n_clusters=2, max_iters=100, seeding='d2', alpha=0.5, tol=1e-4, verbose=0, random_state=42):
         self.n_clusters = n_clusters
         self.max_iters = max_iters
         self.tol = tol
         self.inertia = list()
         self.labels = None
+        self.alpha = alpha
+        self.seeding = seeding
+        self.verbose = verbose
         self.random_state = random_state
         
     def fit(self, X):
         np.random.seed(self.random_state)
-        self.centers = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+        if self.seeding == 'random':
+            self.centers = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+        elif self.seeding == 'd2':
+            self.centers = d2_seeding(X, self.n_clusters)
+        else:
+            self.centers = pca_aggregate(X, self.alpha)
+            
         simulate_weights = np.ones(X.shape[0], dtype=np.float64)
         
         for _iter in range(self.max_iters):
@@ -31,7 +41,12 @@ class StandardKMeans:
             self.inertia.append(_inertia)
             self.centers = updated_centers
             
-            print(_iter)
+            if self.verbose:
+                print("iteration:", _iter+1)
+                
+        if self.verbose:
+            print("clusters:", self.centers.shape[0])
+            
         self.labels = self.labels_assignment(X)
         
         
@@ -41,7 +56,7 @@ class StandardKMeans:
         return np.argmin(dists, axis=1)
     
     def centers_update(self, X, labels):
-        new_centers = np.array([X[labels == i].mean(axis=0) for i in range(self.n_clusters)])
+        new_centers = np.array([X[labels == i].mean(axis=0) for i in range(self.centers.shape[0])])
         return new_centers
 
     
