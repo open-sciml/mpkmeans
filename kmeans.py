bf460528e1c53d072de03cf3fc7c6748e99a4ae2
the null <xinyechenai@gmail.com>
2024-01-20 10:28:17 +0100

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -6,7 +6,7 @@ from seeding import *
 
 """
 StandardKMeans1: the native kmeans algorithm using distance 1
-StandardKMeans2: the native kmeans algorithm using distance 1
+StandardKMeans2: the native kmeans algorithm using distance 2
 mp1KMeans: the mixed precision kmeans algorithm using distance 1
 mp2KMeans: the mixed precision kmeans algorithm using distance 2
 allowKMeans1: kmeans performed in full low precision using distance 1
